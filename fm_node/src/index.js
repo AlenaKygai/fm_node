4b06f654385809e9e6422496a24e1ac9c945166e
AlenaKygai <alena.kygai@gmail.com>
2022-03-11 20:23:57 +0300

    Add ESModules

--- fm_node/src/index.js
+++ fm_node/src/index.js
@@ -1,7 +1,10 @@
-require('./style.css');
-const Component = require('./Component');
+import './style.css'
+import Component from './Component';
 
 console.log('test');
 const resultOfSumTwoNumber = 5+3;
 console.log(resultOfSumTwoNumber);
 const comp = new Component(12);
+console.log(comp.render());
+console.log('test');
+console.log('test');
