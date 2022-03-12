13199a03c9e4ac26912623cee5f9a53ea2a4b9a3
AlenaKygai <alena.kygai@gmail.com>
2022-03-12 21:50:37 +0300

    Doing cache

--- fm_node/src/math.js
+++ fm_node/src/math.js
@@ -1,13 +1,13 @@
+const cache = new Map();
+
 export const pow =(base,exp) =>{
-    return base**exp;
+   const key = `${base}_${exp}`;
+   if(!cache.has(key)){
+    cache.set(key, base**exp);
+   }
+   return cache.get(key);
 }
 
 export const mult = (a,b) => a*b;
 
 export const sum = (...args) => args.reduce((res,arg)=>res+arg,0);
-
-const superSecret = 'Elon Musk';
-
-const ssFunction = ()=> superSecret;
-
-export default ssFunction;
